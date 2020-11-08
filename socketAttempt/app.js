d66f419bf0003cd431349f13f3391394b2c9ce9a
Bharddwaj Vemulapalli <bharddwajvemulapalli@gmail.com>
2020-11-07 21:26:03 -0500

    the socket tutorial now works

--- socketAttempt/app.js
+++ socketAttempt/app.js
@@ -24,11 +24,13 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-    io.emit('Message:', msg);
-  });
   
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+      });
 });
 
 http.listen(3000, () => {
